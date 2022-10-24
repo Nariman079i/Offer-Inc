9da95cf783591350fb3080e2bb1512b8eb3928f2
Nariman <Nariman079i>
2022-10-24 16:10:14 +0300

    edit API for Industries

--- users/serializer.py
+++ users/serializer.py
@@ -1,16 +1,41 @@
-from rest_framework.serializers import *
+import io
+
+from rest_framework import  serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from .models import *
 
+class LocateModel:
+    def __init__(self  , region , title, population):
 
-class IndustrySerializer(ModelSerializer):
+        self.region = region
+        self.title = title
+        self.population = population
+
+
+class LocateSerializerNoModel(serializers.Serializer):
+    region = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    population = serializers.IntegerField()
+
+
+def encode():
+    model = LocateModel(18,'Хамамат-Юрт', 44)
+    model_sr = LocateSerializerNoModel(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+class IndustrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Industry
         fields = '__all__'
 
 
-class LocateSerializer(ModelSerializer):
-    region = ReadOnlyField(source='region_id.title')
+class LocateSerializer(serializers.ModelSerializer):
+    region = serializers.ReadOnlyField(source='region_id.title')
     class Meta:
         model = Locate
-        fields = ['id' ,'region', 'title', 'population']
+        fields = ['id','region', 'title', 'population']
--- users/views.py
+++ users/views.py
@@ -7,21 +7,14 @@ from .models import *
 from .serializer import *
 from rest_framework.generics import *
 
-class IndustriesApiList(APIView):
-    def get(self, request, count ,string):
-        industies = Industry.objects.filter(title__contains=string)[:count].values()
-        return Response({'data': list(industies)})
-
-    def post(self, request):
-
-        industies_new = Industry.objects.create(
-            title=request.data['title']
-        )
-        return Response({'data':model_to_dict(industies_new)})
+class IndustriesApiList(ListAPIView):
+    queryset = Industry.objects.all()
+    serializer_class = IndustrySerializer
 
 class LocateApiList(APIView):
 
     def get(self, request, count ,string):
         locate = Locate.objects.filter(title__contains=string).order_by('-title').order_by("-population")[:count].values()
+
         return Response({'data': list(locate)})
 
